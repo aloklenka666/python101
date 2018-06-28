3bb559da39f959ab008b2368d7c7469f8daef675
aloklenka666 <aloklenka666@gmail.com>
2018-06-28 11:41:37 +0530

    python programm print & comment

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("hello world!")
+print("hellow again!")
+print("i like typing this")
+print("this is fun")
+print('yqy! printing')
+print("i'd much rather you 'not'")
+print('i"said"do not touch this')
---
+++ ex2.py
@@ -0,0 +1,8 @@
+#A comment , this is so you can read your program later.
+#Anything after the # is ignored by python.
+print("i could have code like this.") # and the comment after is ignored 5
+# you can also use acomment to "disable" or comment out code: 
+#print("this won't run.")
+print("this will run.")
+print("hi # there.")
+print('hi # there.')
