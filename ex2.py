c54baab67211f004fe31a4dcb50ca3f6d228d882
aloklenka666 <aloklenka666@gmail.com>
2018-07-01 12:29:05 +0530

    H/w 2

--- ex2.py
+++ ex2.py
@@ -6,5 +6,10 @@ print("i could have code like this.") # and the comment after is ignored 5
 print("this will run.")
 print("hi # there.")
 print('hi # there.')
+#h/w
+#using triple quotes string can be serve as multiline comments
 '''python is a object oriented scripting language
 python is a interpritate language'''
+print('this is another line')
+print("this is only one of the line")
+#here '#' is consider as string
