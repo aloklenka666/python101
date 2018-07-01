370053ead39ecfa51da38290bd85ce8e3e5d58a1
aloklenka666 <aloklenka666@gmail.com>
2018-07-01 12:59:56 +0530

    H/w 3.Numbers and math

---
+++ ex3.py
@@ -0,0 +1,15 @@
+# + -> addition operation
+# - -> substraction operation
+# / -> division operation
+# * -> multiplication operation
+# % -> modules operation ( reminder)
+# < -> less than 
+# > -> greater than
+# <= -> less than equal
+# >= -> greater than equal
+print(5+6*4/2-5)
+print(10%2)
+print(5>6)
+print(3<5)
+print(5<=3)
+print(7>=4)
